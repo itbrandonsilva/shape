9eb8650799fda87a87cd947a611242a7b5825a26
Brandon Silva <itbrandonsilva@gmail.com>
2016-12-08 03:58:55 -0500

    Correct name.

--- typedoc.js
+++ typedoc.js
@@ -2,7 +2,7 @@ module.exports = {
     target: "ES6",
     theme: "./node_modules/typedoc-default-themes/bin/default",
     readme: "none",
-    name: "Vector2 Documentation",
+    name: "Shape Documentation",
     excludePrivate: true,
     exclude: "**/+(demo|node_modules)/**",
     mode: "file"
